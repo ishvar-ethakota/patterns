908924580842511ae928273c6d3afc6a8c23cf94
ishvar-ethakota <ishvarethakota777@gmail.com>
2025-08-19 18:53:57 +0530

    ram charan

--- patt13.java
+++ patt13.java
@@ -1,5 +1,28 @@
 public class patt13 {
     public static void main(String[] args) {
-        
+        int n = 6;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=n;j>=i;j--)
+            {
+                System.out.print(" ");
+            }
+            for(int k=1;k<=n;k++)
+            {
+                if(k==1||k==i)
+                {
+                    System.out.print("*");
+                }
+                else if(i==n)
+                {
+                    System.out.print("**");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.print("\n");
+            
+        }
     }
 }
--- patt9.java
+++ patt9.java
@@ -1,13 +1,13 @@
 public class patt9 {
     public static void main(String[] args) {
         int n = 10;
-        for(int i=0;i<n;i++)
+        for(int i=1;i<=n;i++)
         {
-            for(int j=0;j<=i;j++)
+            for(int j=1;j<=i;j++)
             {
                 System.out.print(" ");
             }
-            for(int j=0;j<(2*n-1)-2*i;j++)
+            for(int j=1;j<(2*n)-2*i;j++)
             {
                 System.out.print("*");
             }
